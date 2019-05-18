877d4201f327d2d07a55a411f54398202c6721ed
PaViGa <valiantigor@gmail.com>
2019-05-18 15:55:47 +0300

    Fix anchors

---
+++ Tap-to-kill game V.2/Assets/Scripts/ConectToServer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ConectToServer : MonoBehaviour
+{
+    [SerializeField]
+    Image img;
+    [SerializeField]
+    GameObject panel;
+    [SerializeField]
+    Text LoadProcent;
+
+    void Update()
+    {
+        img.DOFillAmount(1, Random.Range(1,20));
+        LoadProcent.text = Mathf.Round(img.fillAmount * 100).ToString();
+        if(img.fillAmount >= 0.99f)
+        {
+            panel.SetActive(true);
+            img.gameObject.SetActive(false);
+            Destroy(this.gameObject);
+        }
+
+    }
+}
